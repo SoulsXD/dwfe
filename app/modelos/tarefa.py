af8d16d6793f9c39723184fc74c7a83e2b089cd1
Marcelo <165174662+SoulsXD@users.noreply.github.com>
2025-06-04 07:41:28 -0300

    deletar para recomeçar

--- app/modelos/tarefa.py
+++
@@ -1,7 +0,0 @@
-from app import banco
-
-class Tarefa(banco.Model):
-    id = banco.Column(banco.Integer, primary_key=True)
-    descricao = banco.Column(banco.String(200))
-    feita = banco.Column(banco.Boolean, default=False)
-    lista_id = banco.Column(banco.Integer, banco.ForeignKey('lista.id'))
--- app/modelos/usuario.py
+++
@@ -1,7 +0,0 @@
-from app import banco
-from flask_login import UserMixin
-
-class Usuario(UserMixin, banco.Model):
-    id = banco.Column(banco.Integer, primary_key=True)
-    nome = banco.Column(banco.String(100), unique=True)
-    senha = banco.Column(banco.String(100))
