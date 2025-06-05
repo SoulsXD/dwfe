a27c74aac248fb231272ffcf1dffeb8f47e5dc38
Marcelo <165174662+SoulsXD@users.noreply.github.com>
2025-06-05 18:13:27 -0300

    Delete tests directory

--- tests/data.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO user (username, password)
-VALUES ('test', 'pbkdf2:sha256:600000$ZIXoD5JP0HuZZ0u2$73520603a2a42245cbf0e2de6fe0e2fffc3c5e5853ef582bafe884aeae7df52c');
-
-INSERT INTO lista (nome, user_id)
-VALUES ('Lista de Teste', 1);
-
-INSERT INTO tarefa (descricao, concluida, lista_id)
-VALUES ('Tarefa de Teste', 0, 1);
