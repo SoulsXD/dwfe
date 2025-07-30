06969b33221261919f837220dbb4041d6b8c597c
Marcelo <165174662+SoulsXD@users.noreply.github.com>
2025-07-30 15:57:24 -0300

    Update test_db.py

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,6 +1,7 @@
 from flaskr.db import get_db
-
+#testa o banco de dados
 def test_get_db(app):
     with app.app_context():
         db = get_db()
-        assert db is not None
+        assert db.is_connected()  # garante que conexão foi aberta
+        db.close()
